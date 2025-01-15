b0e9a38f0b1881142ca87f09d3c8b890fdeabb61
Thaneshwor Joshi <thaneshwor10@gmail.com>
2025-01-15 20:14:00 +0545

    feat: Add navbar component

--- src/app/documents/[documentID]/page.tsx
+++ src/app/documents/[documentID]/page.tsx
@@ -1,4 +1,5 @@
 import Editor from './editor'
+import { NavBar } from './navbar';
 import Toolbar from './toolbar';
 
 interface DocumentPageProps {
@@ -7,12 +8,16 @@ interface DocumentPageProps {
 
 const DocumentPage = async ({ params }: DocumentPageProps) => {
     const { documentID } =  await params;
-    
-    console.log(documentID)
+
     return (
         <div className='min-h-screen bg-[#FAFBFD]'>
-            <Toolbar />
-            <Editor />
+            <div className='flex flex-col px-4 pt-2 gap-y-2 fixed tap-0 left-0 right-0 z-10 bg-[#FAFBFD]'>
+                <NavBar />
+                <Toolbar />
+            </ div>
+            <div className='pt-[114px] print:pt-0'>
+                <Editor />
+            </div>
         </div>
     );  
 }
--- src/app/documents/[documentID]/toolbar.tsx
+++ src/app/documents/[documentID]/toolbar.tsx
@@ -600,7 +600,7 @@ const Toolbar: React.FC = () => {
 
 
     return (
-        <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+        <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto print:hidden">
             {sections[0].map((item) => (<ToolbarButton key={item.label} {...item} disabled={!editor}/>))}
             <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
             {/* TODO: Font family */}
