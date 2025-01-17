c3da03f4e15a8c20003d81c1a28b2f862e61dba6
Thaneshwor Joshi <thaneshwor10@gmail.com>
2025-01-17 18:18:17 +0545

    feat: Authentication setup (Clerk)

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const config = {
+    providers: [
+        {
+            domain: "https://possible-flamingo-24.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+};
+
+export default config;
