d7dc7ce0343fb945bff75583cbc59374d7bf1dc4
Thaneshwor Joshi <thaneshwor10@gmail.com>
2025-01-27 17:36:17 +0545

    add: Collabrative margin ruler

--- src/app/documents/[documentID]/editor.tsx
+++ src/app/documents/[documentID]/editor.tsx
@@ -19,6 +19,7 @@ import { Color } from '@tiptap/extension-color'
 import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
 
+import { useStorage } from '@liveblocks/react/suspense';
 
 import { useEditorStore } from '@/store/use-editior-store';
 import { FontSizeExtension } from '@/extensions/font-size';
@@ -29,6 +30,9 @@ import { Threads } from './threads';
 export const Editor = () => {
     const { setEditor } = useEditorStore();
     const liveblocks = useLiveblocksExtension();
+
+    const leftMargin = useStorage((root) => root.leftMargin);
+    const rightMargin = useStorage((root) => root.rightMargin);
     
     const editor = useEditor({
         immediatelyRender: false,
@@ -58,7 +62,7 @@ export const Editor = () => {
         },
         editorProps: {
             attributes: {
-                style: 'padding-left: 56px; padding-right:56px;',
+                style: `padding-left: ${leftMargin ?? 56}px; padding-right:${rightMargin ?? 56}px;`,
                 class: 'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
             },
         },
